00672abf01ce12b8a7f46d964c730a0c455d0dda
Chris <chrisroberts1080@gmail.com>
2024-11-21 14:10:35 +0000

    Multithread simulations

--- agents/CPPAgent/Board.cpp
+++ agents/CPPAgent/Board.cpp
@@ -5,13 +5,12 @@
 #include "Board.h"
 #include "Tile.h"
 #include <sstream>
+#include <set>
+#include <stack>
 
 Board::Board(std::vector<std::vector<Tile>> state_vec, const int &size,
              std::string winner)
     : state(state_vec), size(size), winner(winner) {}
-
-Board::Board(const Board &board, const int &size, std::string winner)
-    : state(board.state), size(size), winner(winner) {}
 
 std::vector<std::vector<Tile>> &Board::get_state() { return this->state; }
 
@@ -29,4 +28,79 @@ std::vector<std::pair<int, int>> Board::get_moves() const {
     }
   }
   return choices;
+}
+
+Board Board::duplicate() { return Board(this->state, this->size, this->winner); }
+
+std::string Board::has_ended() {
+  int size = 11;
+  std::set<std::pair<int, int>> visited;
+
+  std::vector<Tile> red_tiles;
+  std::vector<Tile> blue_tiles;
+
+  for (int idx = 0; idx < size; ++idx) {
+    if (state[0][idx].getColour() == "R") {
+      red_tiles.push_back(state[0][idx]);
+    }
+    if (state[idx][0].getColour() == "B") {
+      blue_tiles.push_back(state[idx][0]);
+    }
+  }
+
+  // Check red tiles
+  for (const auto &tile : red_tiles) {
+    if (visited.insert({tile.getX(), tile.getY()}).second) {
+      if (DFSColour(0, tile.getY(), "R", visited) == "R") {
+        return "R";
+      }
+    }
+  }
+
+  // Check blue tiles
+  for (const auto &tile : blue_tiles) {
+    if (visited.insert({tile.getX(), tile.getY()}).second) {
+      if (DFSColour(tile.getX(), 0, "B", visited) == "B") {
+        return "B";
+      }
+    }
+  }
+
+  return "";
+}
+
+std::string Board::DFSColour(int x, int y, const std::string &colour, std::set<std::pair<int, int>> &visited) {
+  int size = 11;
+
+  std::stack<std::pair<int, int>> stack;
+  stack.push({x, y});
+  visited.insert({x, y});
+
+  while (!stack.empty()) {
+    std::pair<int, int> coordinate = stack.top();
+    stack.pop();
+
+    // Win conditions
+    if (colour == "R" && coordinate.first == size - 1) {
+      return "R";
+    }
+    if (colour == "B" && coordinate.second == size - 1) {
+      return "B";
+    }
+
+    // Visit neighbours
+    for (int i = 0; i < Tile::NEIGHBOUR_COUNT; ++i) {
+      int x_n = coordinate.first + Tile::I_DISPLACEMENTS[i];
+      int y_n = coordinate.second + Tile::J_DISPLACEMENTS[i];
+
+      if (x_n >= 0 && x_n < size && y_n >= 0 && y_n < size) {
+        if (visited.find({x_n, y_n}) == visited.end() &&
+            state[x_n][y_n].getColour() == colour) {
+          visited.insert({x_n, y_n});
+          stack.push({x_n, y_n});
+        }
+      }
+    }
+  }
+  return "";
 }
--- agents/CPPAgent/MCTSNode.cpp
+++ agents/CPPAgent/MCTSNode.cpp
@@ -17,6 +17,8 @@
 #define DECAY_RATE -0.05
 #include <iostream>
 
+auto rng = std::default_random_engine {};
+
 inline std::string opp(const std::string &c) { return c == "R" ? "B" : "R"; }
 
 std::vector<std::pair<int, int>> get_valid_moves(const std::string state,
@@ -47,18 +49,18 @@ MCTSNode::MCTSNode(const Board &state, const std::string colour,
 void MCTSNode::generate_all_children_nodes() {
   for (std::pair<int, int> move : this->valid_moves) {
     // Make a new board and make the move
-    Board brd(this->state);
-    brd.make_move(move, this->colour);
-    MCTSNode *new_node = new MCTSNode(brd, this->colour, this, move);
-    this->children.push_back(new_node);
+    Board brd = state.duplicate();
+    brd.make_move(move, colour);
+    MCTSNode *new_node = new MCTSNode(brd, colour, this, move);
+    children.push_back(new_node);
   }
 }
 
 void MCTSNode::delete_children() {
-  for (MCTSNode *child : this->children) {
+  for (MCTSNode *child : children) {
     delete child;
   }
-  this->children.clear();
+  children.clear();
 }
 
 void MCTSNode::backpropagate(double result, int visits) {
@@ -71,84 +73,8 @@ void MCTSNode::backpropagate(double result, int visits) {
 }
 
 std::pair<int, int> MCTSNode::get_best_move() {
-  MCTSNode *best_node = this->best_child(0.0);
+  MCTSNode *best_node = best_child(0.0);
   return best_node->move;
-}
-
-std::string MCTSNode::has_ended() {
-  std::vector<std::vector<Tile>> &tiles = this->state.get_state();
-  int size = 11;
-  std::set<std::pair<int, int>> visited;
-
-  std::vector<Tile> red_tiles;
-  std::vector<Tile> blue_tiles;
-
-  for (int idx = 0; idx < size; ++idx) {
-    if (tiles[0][idx].getColour() == "R") {
-      red_tiles.push_back(tiles[0][idx]);
-    }
-    if (tiles[idx][0].getColour() == "B") {
-      blue_tiles.push_back(tiles[idx][0]);
-    }
-  }
-
-  // Check red tiles
-  for (const auto &tile : red_tiles) {
-    if (visited.insert({tile.getX(), tile.getY()}).second) {
-      if (DFSColour(0, tile.getY(), "R", visited) == "R") {
-        return "R";
-      }
-    }
-  }
-
-  // Check blue tiles
-  for (const auto &tile : blue_tiles) {
-    if (visited.insert({tile.getX(), tile.getY()}).second) {
-      if (DFSColour(tile.getX(), 0, "B", visited) == "B") {
-        return "B";
-      }
-    }
-  }
-
-  return "";
-}
-
-std::string MCTSNode::DFSColour(int x, int y, const std::string &colour,
-                                std::set<std::pair<int, int>> &visited) {
-  int size = 11;
-  std::vector<std::vector<Tile>> &tiles = this->state.get_state();
-
-  std::stack<std::pair<int, int>> stack;
-  stack.push({x, y});
-  visited.insert({x, y});
-
-  while (!stack.empty()) {
-    std::pair<int, int> coordinate = stack.top();
-    stack.pop();
-
-    // Win conditions
-    if (colour == "R" && coordinate.first == size - 1) {
-      return "R";
-    }
-    if (colour == "B" && coordinate.second == size - 1) {
-      return "B";
-    }
-
-    // Visit neighbours
-    for (int i = 0; i < Tile::NEIGHBOUR_COUNT; ++i) {
-      int x_n = coordinate.first + Tile::I_DISPLACEMENTS[i];
-      int y_n = coordinate.second + Tile::J_DISPLACEMENTS[i];
-
-      if (x_n >= 0 && x_n < size && y_n >= 0 && y_n < size) {
-        if (visited.find({x_n, y_n}) == visited.end() &&
-            tiles[x_n][y_n].getColour() == colour) {
-          visited.insert({x_n, y_n});
-          stack.push({x_n, y_n});
-        }
-      }
-    }
-  }
-  return "";
 }
 
 double MCTSNode::simulate_from_node(std::string current_colour) {
@@ -157,30 +83,28 @@ double MCTSNode::simulate_from_node(std::string current_colour) {
   // Check if the game has ended at this node if so there is a winning move
   // The reward here is higher than is possible for other moves which guarantees
   // that the winning move is chosen
-  std::string winner = this->has_ended();
-  if (winner == this->colour) {
+  Board local_state = state.duplicate();
+  std::vector<std::pair<int, int>> local_moves = local_state.get_moves();
+
+  std::string winner = local_state.has_ended();
+  if (winner == colour) {
     return 2;
   }
 
-  std::default_random_engine rng(time(0));
-
   // Shuffle the vector
   // Faster than picking a random element from the vector every iteration
-  std::shuffle(valid_moves.begin(), valid_moves.end(), rng);
+  std::shuffle(local_moves.begin(), local_moves.end(), rng);
   int i = 0;
   while (true) {
-    std::pair<int, int> move = valid_moves[i];
+    std::pair<int, int> move = local_moves[i];
     i++;
     moves_taken.push_back(move);
 
-    this->state.make_move(move, current_colour);
-    winner = this->has_ended();
+    local_state.make_move(move, current_colour);
+    winner = local_state.has_ended();
     if (winner != "") {
-      for (auto &move : moves_taken) {
-        this->state.make_move(move, "0");
-      }
       return (std::exp((long double)(DECAY_RATE * moves_taken.size())) *
-              (this->colour == current_colour ? 1 : -1));
+              (colour == current_colour ? 1 : -1));
     }
     current_colour = opp(current_colour);
   }
@@ -189,9 +113,9 @@ double MCTSNode::simulate_from_node(std::string current_colour) {
 MCTSNode *MCTSNode::best_child(float c) {
   MCTSNode *best_node = nullptr;
   double best_score = -std::numeric_limits<double>::infinity();
-  double log_parent_visits = std::log(this->visits);
+  double log_parent_visits = std::log(visits);
 
-  for (MCTSNode *child : this->children) {
+  for (MCTSNode *child : children) {
     // If the child has not been visited, return it immediately
     if (child->visits == 0) {
       return child;
