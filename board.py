ba21796c198cc2e35407a7dcf3cb3adbd383b80b
James Downes <james.downes912@gmail.com>
2024-11-26 13:51:32 +0000

    Fully implement abandon functionality

--- board.py
+++ board.py
@@ -2,6 +2,8 @@ import template
 from tile import Tile
 from src.Colour import Colour
 from constants import Player, Neighbour
+from rich.console import Console
+from rich.text import Text
 
 class Board:
     def __init__(self, opponentColour):
@@ -50,7 +52,36 @@ class Board:
                 output += f"{node.row} "
             output += "\n"
 
-        return output                    
+        return output
+
+    def showAbandoned(self) -> str:
+        """
+        Print the board with colours using rich to colour the characters
+        """
+        console = Console()
+        output = ""
+        leading_spaces = ""
+        for row in self.array:
+            text_line = Text(leading_spaces)  # Start with leading spaces
+            leading_spaces += " "
+            for node in row:
+                if node.occupiedBy == Player.SELF:
+                    if self.opponentColour == Colour.RED:
+                        text_line.append("B" + " ", style="bold blue")
+                    else:
+                        text_line.append("R" + " ", style="bold red")
+                elif node.occupiedBy == Player.OPPONENT:
+                    if self.opponentColour == Colour.RED:
+                        text_line.append("R" + " ", style="bold red")
+                    else:
+                        text_line.append("B" + " ", style="bold blue")
+                elif node.abandoned:
+                    text_line.append("A" + " ", style="bold orange1")
+                else:
+                    text_line.append("0" + " ", style="bold white")
+
+            output += text_line.plain + "\n"  # Append line to output
+            console.print(text_line)  # Print the line with colors
 
     def getNode(self,x,y):
         if x < 0 or x > 10 or y < 0 or y > 10:
@@ -67,17 +98,27 @@ class Board:
 
     def evaluate(self, node): 
         if (abs(node.row) == 2):
-            self.compare(template.row2Template.node, node)
+            if (self.compare(template.row2Template.node, node)):
+                self.abandon(node, template.row2Template.node)
         
 
     def compare(self, templateNode, node):
-        for neighbour in range(Neighbour):
+        for neighbour in Neighbour:
             if templateNode.neighbours[neighbour] == None:
                 continue
             elif templateNode.neighbours[neighbour].occupiedBy != node.neighbours[neighbour].occupiedBy:
                 return False
             elif templateNode.neighbours[neighbour].occupiedBy == node.neighbours[neighbour].occupiedBy:
                 self.compare(templateNode.neighbours[neighbour], node.neighbours[neighbour])
+        return True
+    
+    def abandon(self, node, templateNode):
+        for neighbour in Neighbour:
+            if templateNode.neighbours[neighbour] == None:
+                continue
+            else:
+                node.neighbours[neighbour].abandoned = True
+                self.abandon(node.neighbours[neighbour], templateNode.neighbours[neighbour])
         return True
                 
     def print_board(self) -> str:
@@ -99,5 +140,17 @@ class Board:
         
     # def isWinnable(self):
 
+board = Board(Colour.RED)
+print(board.rowNumbers())
+board.opponentMove(6,9)
+board.opponentMove(4,9)
+board.opponentMove(4,1)
+
 board = Board(Colour.BLUE)
 print(board.rowNumbers())
+board.opponentMove(9,6)
+board.opponentMove(9,4)
+board.opponentMove(1,4)
+
+
+board.showAbandoned()
