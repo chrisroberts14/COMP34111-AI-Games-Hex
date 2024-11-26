3954f789ae3142fbfd043cfe254da61f8bf7f280
Chris <chrisroberts1080@gmail.com>
2024-11-26 16:06:36 +0000

    Faster cpp implementation

--- agents/Group25/MCTSNode.h
+++ agents/Group25/MCTSNode.h
@@ -5,33 +5,33 @@
 #ifndef MCTSNODE_H
 #define MCTSNODE_H
 
-#include "Board.h"
 #include <random>
-#include <stack>
+#include <set>
 
 class MCTSNode {
   std::string colour;
-  Board state;
   MCTSNode *parent;
   std::vector<MCTSNode *> children;
   int visits;
   double payoff_sum;
   std::pair<int, int> move;
-  std::vector<std::pair<int, int>> valid_moves;
+  std::set<std::pair<int, int>> blue_moves;
+  std::set<std::pair<int, int>> red_moves;
+  std::set<std::pair<int, int>> open_moves;
+
 
 public:
-  MCTSNode(const Board &state, std::string colour, MCTSNode *parent,
+  MCTSNode(std::set<std::pair<int, int>> red_moves, std::set<std::pair<int, int>> blue_moves, std::set<std::pair<int, int>> open_moves, std::string colour, MCTSNode *parent,
            std::pair<int, int> move);
 
   void backpropagate(double result, int visits = 1);
   MCTSNode *best_child(float c);
-  [[nodiscard]] double simulate_from_node(std::string current_colour, int turn_number) const;
+  [[nodiscard]] double simulate_from_node();
   void generate_all_children_nodes();
   std::pair<int, int> get_best_move();
   [[nodiscard]] MCTSNode *get_child(int index) const;
   [[nodiscard]] unsigned long get_children_size() const;
   void delete_children();
-    [[nodiscard]] int get_turn_number() const;
 };
 
 #endif // MCTSNODE_H
