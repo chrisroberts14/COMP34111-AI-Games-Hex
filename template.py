900d9b4bcacef4335b9eab48d3173a231bff2cb4
James Downes <james.downes912@gmail.com>
2024-11-27 14:15:54 +0000

    Refactor template.py
    Placed Template class at the top of the file to aid understanding

--- template.py
+++ template.py
@@ -1,5 +1,10 @@
 from constants import Player
 
+class Template:
+    def __init__(self, row, tile_of_origin: 'TemplateTile'):
+        self.row = row
+        self.tile_of_origin = tile_of_origin
+
 class TemplateTile:
     def __init__(self, north_west_neighbour=None, north_east_neighbour=None, east_neighbour=None, south_east_neighbour=None, south_west_neighbour=None, west_neighbour=None):
         self.neighbours = [north_west_neighbour, north_east_neighbour, east_neighbour, south_east_neighbour, south_west_neighbour, west_neighbour]
@@ -18,11 +23,6 @@ class PlayerTile(TemplateTile):
     def __init__(self, north_west_neighbour=None, north_east_neighbour=None, east_neighbour=None, south_east_neighbour=None, south_west_neighbour=None, west_neighbour=None):
         super().__init__(north_west_neighbour, north_east_neighbour, east_neighbour, south_east_neighbour, south_west_neighbour, west_neighbour)
         self.occupied_by = Player.SELF
-
-class Template:
-    def __init__(self, row, tile_of_origin):
-        self.row = row
-        self.tile_of_origin = tile_of_origin
 
 row2Template = Template(2, OpponentTile(south_west_neighbour=EmptyTile(), south_east_neighbour=EmptyTile()))
 
