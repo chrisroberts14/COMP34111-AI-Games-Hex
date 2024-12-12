5e1fb2f122376a467197d28a1227f9824dd40fd0
Robert Chiru <robert.chiru5@gmail.com>
2024-12-12 11:59:33 +0000

    removed unnecessary files

--- agents/Group25-AZ/azsrc/hexhex/__main__.py
+++
@@ -1,4 +0,0 @@
-import hexhex.interactive.interactive
-
-if __name__ == '__main__':
-    hexhex.interactive.interactive.main()
--- agents/Group25-AZ/azsrc/hexhex/utils/summary.py
+++
@@ -1,6 +0,0 @@
-from torch.utils.tensorboard import SummaryWriter
-
-writer = SummaryWriter()
-
-layout = {'training': {'val/train loss': ['Multiline', ['train/train_loss', 'train/val_loss']]}}
-writer.add_custom_scalars(layout)
